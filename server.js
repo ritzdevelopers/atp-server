b5b88a4030604689e65c87b4356911c1b390169a
itzz-aryan-121 <arhanrizvi9@gmail.com>
2026-04-20 12:57:21 +0530

    first commit

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
