a604094bab080b412f891e90577d7f98d0bfe886
itzz-aryan-121 <aryan.22b0131042@abes.ac.in>
2026-04-20 12:53:10 +0530

    Setup Express Server

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mysql from "mysql2";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
