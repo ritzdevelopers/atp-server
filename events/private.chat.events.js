f8c3e65b030580f629ccefd0e583f53dbf617e58
itzz-aryan-121 <arhanrizvi9@gmail.com>
2026-06-18 17:36:03 +0530

    write socket code for detecting typing indicator

--- events/private.chat.events.js
+++ events/private.chat.events.js
@@ -5,12 +5,12 @@ import {
 } from "../controllers/chats/sockets/private.chat.controller.js";
 import { seen_private_message_controller } from "../controllers/chats/sockets/seen_private_message.controller.js";
 
-export const register_private_chat_events = (io, socket) => {
+export const register_private_chat_events = (io, socket) => { 
   socket.on("join_user_room", (userId) => {
     if (userId == null || userId === "") return;
     socket.join(`user:${userId}`);
   });
-
+  
   socket.on("join_chat_room", (chat_id, user_id) => {
     if (!chat_id || !user_id) return;
     socket.data.user_id = user_id;
@@ -29,18 +29,36 @@ export const register_private_chat_events = (io, socket) => {
     }
   });
 
+
+  /*------------------ Typing Indicator Events ------------------*/
+  socket.on("typing_indicator", (data)=>{
+    const { chat_id, user_id, receiver_id } = data;
+    if (!chat_id || !user_id || !receiver_id) return;
+     socket.to(`user:${receiver_id}`).emit("typing_indicator", { chat_id, user_id, typing: true });
+  });
+
+  socket.on("typing_stop", (data)=>{
+    const { chat_id, user_id, receiver_id } = data;
+    if (!chat_id || !user_id || !receiver_id) return;
+    socket.to(`user:${receiver_id}`).emit("typing_stop", { chat_id, user_id, typing: false });
+  });
+
+  /*------------------ Private Message Controller ------------------*/
   socket.on("send_private_message", async (data) => {
     await send_private_message_controller(io, socket, data);
   });
 
+  /*------------------ Single Chat History Controller ------------------*/
   socket.on("get_single_chat_history", async (data) => {
     await get_single_chat_history_controller(io, socket, data);
   });
 
+  /*------------------ Seen Private Message Controller ------------------*/
   socket.on("seen_private_message", async (data) => {
     await seen_private_message_controller(io, socket, data);
   });
 
+  /*------------------ All Messages Controller ------------------*/
   socket.on("get_all_messages", async (data) => {
     await get_all_messages_controller(io, socket, data);
   });
